0258dff953ed55706142efcab6bb105c98baaa26
CleverDocsApp <info@clic360.com>
2025-06-13 15:07:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,12 @@ function App() {
                 <div className="absolute -top-12 -right-12 w-24 h-24 bg-primary opacity-10 rounded-full blur-3xl animate-pulse-slow hidden md:block"></div>
               </h1>
               <p className="text-lg md:text-xl lg:text-2xl text-gray-200 max-w-3xl mx-auto leading-relaxed">
-                The intelligent assistant that helps you chart faster—without compromising clinical quality.<br />{' '}
-                <span style={{ color: '#20BDAA', fontWeight: 'bold' }}>
-                  Built for mental health professionals
-                </span>
+                The intelligent assistant that helps you chart faster—without compromising clinical quality.
+                </p>
+              <p className="text-center text-lg md:text-xl lg:text-2xl text-gray-200 max-w-3xl mx-auto leading-relaxed">
+                 <span style={{ color: '#20BDAA', fontWeight: 'bold' }}>
+                Built for mental health professionals
+                </span>{' '}
                  who want documentation that’s compliant, coherent, and easy to complete.<br />
               </p>
             </div>
