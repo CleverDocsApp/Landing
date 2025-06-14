e9fc63f14147ca4d74ef9012053b09c9564fc99c
CleverDocsApp <info@clic360.com>
2025-06-13 20:52:18 -0400

    Update second metric to +65%

--- src/components/MetricsSection/MetricsSection.tsx
+++ src/components/MetricsSection/MetricsSection.tsx
@@ -17,7 +17,7 @@ const metrics = [
   },
   {
     icon: <TrendingUp size={24} />,
-    value: '+40%',
+    value: '+65%',
     subValue: 'approval increase',
     label: 'Stronger documentation that helps secure insurance approvals.',
     color: 'bg-gradient-to-br from-blue-400 to-blue-600',
