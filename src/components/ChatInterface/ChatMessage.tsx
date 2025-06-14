81f8e3dff6e76804d7a97f81f66ca1a312e7ec83
CleverDocsApp <info@clic360.com>
2025-06-13 21:35:08 -0400

    Light Mode Chat Interface Implementation

--- src/components/ChatInterface/ChatMessage.tsx
+++ src/components/ChatInterface/ChatMessage.tsx
@@ -19,7 +19,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ message }) => {
      {message.sender === 'bot' && (
   <div className="avatar">
     <img
-      src="/images/chat-avatar.svg"
+      src="/images/chat-avatar-inverted.svg"
       alt="On Klinic bot"
       className="chat-avatar"
     />
