210e45c2a78da6d2d585a4c516d562d542d143a2
CleverDocsApp <info@clic360.com>
2026-01-12 14:00:18 -0500

    Updated OkHowToPage.css

--- src/components/OkHowTo/OkHowToHeader.tsx
+++ src/components/OkHowTo/OkHowToHeader.tsx
@@ -46,27 +46,6 @@ const OkHowToHeader: React.FC<OkHowToHeaderProps> = ({ videoCount = 0 }) => {
         </div>
 
         <div className="header-right">
-          {videoCount > 0 && (
-            <div className="video-count-badge">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="video-icon"
-              >
-                <path
-                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span>{videoCount} videos</span>
-            </div>
-          )}
           <a href="/" className="back-button">
             <ArrowLeft size={18} />
             <span className="back-text">Back to Home</span>
